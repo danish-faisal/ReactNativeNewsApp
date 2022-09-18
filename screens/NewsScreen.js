79b911d7bbb8804cff133ec8895568baa72667f6
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-09-18 18:19:27 +0530

    Fetching news in NewsScreen

--- screens/NewsScreen.js
+++ screens/NewsScreen.js
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { View, Text } from "react-native";
+import { NewsContext } from "../API/Context";
 
 const NewsScreen = () => {
+  const {
+    news: { articles },
+  } = useContext(NewsContext);
+
   return (
     <View>
       <Text>News</Text>
