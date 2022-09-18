821281f9d3c2a452442d00c07ab24efe36c71348
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-09-18 14:09:14 +0530

    Setting up header & default tabs

---
+++ components/Tabs.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useWindowDimensions } from "react-native";
+import { SceneMap, TabView } from "react-native-tab-view";
+import DiscoverScreen from "../screens/DiscoverScreen";
+import NewsScreen from "../screens/NewsScreen";
+
+const Tabs = () => {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "first", title: "Discover" },
+    { key: "second", title: "News" },
+  ]);
+
+  const renderScene = SceneMap({
+    first: DiscoverScreen,
+    second: NewsScreen,
+  });
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+};
+
+export default Tabs;
---
+++ screens/DiscoverScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+const DiscoverScreen = () => {
+  return (
+    <View>
+      <Text>Discover</Text>
+    </View>
+  );
+};
+
+export default DiscoverScreen;
